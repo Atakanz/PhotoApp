cc9d3751cf8c0d1e504b695c903e44f0c22cade9
Atakanz <atakanzer@gmail.com>
2022-09-19 23:56:54 +0300

    Conditional navigation form added.

--- PhotoApp/src/Navigation/MainStack.js
+++ PhotoApp/src/Navigation/MainStack.js
@@ -3,53 +3,43 @@ import {NavigationContainer} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
 import {BottomTab} from './BottomTab'
 import {useDispatch, useSelector} from 'react-redux';
-// import {setUser} from '../Management/Features/User/userSlice';
+import {setUser} from '../Management/Features/User/userSlice';
 import LoginStack from './LoginStack';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 export const MainStack = () => {
 
 
-//   const dispatch = useDispatch();
-//   const user = useSelector(state => state.user.user);
-//   const getSavedItem = async () => {
-//     let userData = await AsyncStorage.getItem('savedUser');
-//     const _user = userData ? JSON.parse(userData) : null;
-//     dispatch(setUser(_user));
-//   };
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user.user);
+  const getSavedItem = async () => {
+    let userData = await AsyncStorage.getItem('savedUser');
+    const _user = userData ? JSON.parse(userData) : null;
+    dispatch(setUser(_user));
+  };
   
-//   useEffect(() => {
-//     getSavedItem();
-//   }, [user]);
+  useEffect(() => {
+    getSavedItem();
+  }, [dispatch]);
  
   return (
-//     <NavigationContainer>
-//       <Stack.Navigator screenOptions={{
-//             headerShown: false,
-//           }}>
-//   {!user ? (
-//     <>
-//       <Stack.Screen name='SignIn' component={SignIn} />
-//       <Stack.Screen name='SignUp' component={SignUp} />
-//     </>
-//   ) : (
-//     <>
-//       <Stack.Screen name='BottomTab' component={BottomTab} />
-//     </>
-//   )}
-//   </Stack.Navigator>
-//   </NavigationContainer>
-//   )
-       <NavigationContainer>
-         <Stack.Navigator screenOptions={{
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
             headerShown: false,
           }}>
-            <Stack.Screen name='Login' component={LoginStack} />
-            <Stack.Screen name='BottomTab' component={BottomTab} />
-         </Stack.Navigator>
-       </NavigationContainer>
+  {!user ? (
+    <>
+      <Stack.Screen name='Login' component={LoginStack} />
+    </>
+  ) : (
+    <>
+      <Stack.Screen name='BottomTab' component={BottomTab} />
+    </>
+  )}
+  </Stack.Navigator>
+  </NavigationContainer>
   )
 };
 
