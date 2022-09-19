8afb566df464e0260e63cb8b87f77f7e747d5f45
Atakanz <atakanzer@gmail.com>
2022-09-19 23:57:22 +0300

    Logout added and conditional form tested.

--- PhotoApp/src/Pages/Settings/Settings.js
+++ PhotoApp/src/Pages/Settings/Settings.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import styles from './Settings.style';
+import Buttons from '../../Components/Buttons';
+import {useSelector} from 'react-redux';
+import {firebase} from '../../../config';
+import {logOut} from '../../Management/Features/User/userSlice'
+import {useDispatch} from 'react-redux';
 
 const Settings = () => {
+    const dispatch = useDispatch();
+	const logout = () => {
+		dispatch(logOut(null));
+	}
     return (
        <SafeAreaView>
         <Text>Hello Settings!</Text>
+        <Buttons
+            name="Log out"
+            task={logout}
+          />
        </SafeAreaView>
     )
 }
