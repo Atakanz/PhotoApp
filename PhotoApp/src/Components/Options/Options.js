62f4005b16b713d2e909c41a4ec723f1d319ece9
Atakanz <atakanzer@gmail.com>
2022-09-22 12:00:33 +0300

    Camera-gallery-delete photo option view added.

---
+++ PhotoApp/src/Components/Options/Options.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {SafeAreaView, View, Text, TouchableOpacity} from "react-native";
+import styles from './Options.style';
+
+const Options = ({option1, option2, option3, task1, task2, task3}) => {
+    return (
+        <SafeAreaView style={styles.container}>
+           <TouchableOpacity style={styles.options} onPress={task1}>
+            <Text style={styles.textOption}>{option1}</Text>
+            </TouchableOpacity>
+           <TouchableOpacity style={styles.options} onPress={task2}>
+            <Text style={styles.textOption}>{option2}</Text>
+            </TouchableOpacity>
+           <TouchableOpacity style={styles.options} onPress={task3}>
+            <Text style={styles.textOption}>{option3}</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+}
+
+export default Options;
---
+++ PhotoApp/src/Components/Options/Options.style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        borderWidth: 2,
+        width: '30%',
+        position: 'absolute',
+        top: 30,
+        right: 30,
+        height: 120,
+        borderRadius: 2,
+        borderColor: '#CFF000'
+        },
+    textOption: {
+        fontSize: 14,
+        textAlign: 'center',
+    },
+    options: {
+        height: '33%',
+        justifyContent: 'center',
+    }
+});
