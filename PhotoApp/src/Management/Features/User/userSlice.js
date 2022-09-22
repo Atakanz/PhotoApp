bf226b0a0f78198d683097efb504677386a4186d
Atakanz <atakanzer@gmail.com>
2022-09-22 12:01:25 +0300

    Profile photo reducer-firebase.signOut added

--- PhotoApp/src/Management/Features/User/userSlice.js
+++ PhotoApp/src/Management/Features/User/userSlice.js
@@ -1,8 +1,10 @@
 import {createSlice} from '@reduxjs/toolkit';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {firebase} from '../../../../config';
 
 const initialState = {
   user: [],
+  profilePhoto: null,
 };
 
 export const userSlice = createSlice({
@@ -15,14 +17,19 @@ export const userSlice = createSlice({
     logOut: (state, action) => {
       state.user = action.payload;
       AsyncStorage.removeItem('savedUser');
-      
+      firebase.auth().signOut();
     },
     logIn: (state, action) => {
       state.user = action.payload;
     },
+    setProfilePhoto: (state, action) => {
+      state.profilePhoto = action.payload;
+      firebase.firestore().collection('users')
+		  .doc(firebase.auth().currentUser.uid)
+		  .update({image: action.payload});
   },
-});
+}});
 
-export const {setUser, logIn, logOut} = userSlice.actions;
+export const {setUser, logIn, logOut, setProfilePhoto} = userSlice.actions;
 
 export default userSlice.reducer;
