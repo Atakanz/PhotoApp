31c633d6a4f31d7e453848daef3bc7621e6772bd
Atakanz <atakanzer@gmail.com>
2022-09-22 12:12:21 +0300

    Userinfocard added to settingpage.

--- PhotoApp/src/Pages/Settings/Settings.js
+++ PhotoApp/src/Pages/Settings/Settings.js
@@ -1,27 +1,61 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, View, Image, Text, Pressable, Alert} from 'react-native';
 import styles from './Settings.style';
 import Buttons from '../../Components/Buttons';
+import UserInfoCard from '../../Components/UserInfoCard';
 import {useSelector} from 'react-redux';
-import {firebase} from '../../../config';
-import {logOut} from '../../Management/Features/User/userSlice'
+import {logOut} from '../../Management/Features/User/userSlice';
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import {useDispatch} from 'react-redux';
+import {firebase} from '../../../config';
 
-const Settings = () => {
-    const dispatch = useDispatch();
+const Settings = ({navigation}) => {
+    const theme = useSelector(state => state.theme.theme)
+    const dispatch = useDispatch();     
 	const logout = () => {
 		dispatch(logOut(null));
 	}
+    const colorSelect = theme === 'Dark' ? '#fff' : '#212121';
+	const themeIcon = <Icon name="compare" size={30} 
+    color='#212121' style={{marginRight: 15}} />
+	const editIcon = <Icon name="account-settings" size={30} 
+    color='#212121' style={{marginRight: 15}} />
+	const logOutIcon = <Icon name="logout" size={30} 
+    color='#212121' style={{marginRight: 15}} />
+    
     return (
-       <SafeAreaView>
-        <Text>Hello Settings!</Text>
-        <Buttons
-            name="Log out"
-            task={logout}
+        <SafeAreaView 
+	      style={[styles.container, styles[`container${theme}`]]}>
+        
+         <UserInfoCard navigation={navigation} />
+         
+        <View style={styles.buttons}>
+         <View style={styles.button}>
+          <Buttons
+            name="Theme Settings"
+            icon={themeIcon}
+            task={() => navigation.navigate('ThemeSettingsScreen')}
           />
-       </SafeAreaView>
+          <Buttons
+            name="Edit Profile"
+            icon={editIcon}
+            task={() => navigation.navigate('EditProfileScreen')}
+          />
+		  <View style={styles.logOutButton}>
+             <Buttons
+               name="Log out"
+               icon={logOutIcon}
+               task={logout}
+              />
+		  </View>
+          </View>
+         </View>
+        </SafeAreaView>
     )
 }
 
 export default Settings;
 
+
+
+          
--- PhotoApp/src/Pages/Settings/Settings.style.js
+++ PhotoApp/src/Pages/Settings/Settings.style.js
@@ -1,3 +1,23 @@
 import {StyleSheet} from "react-native";
 
-export default StyleSheet.create({})
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+      },
+      containerDark: {
+        backgroundColor: '#212121',
+      },
+      containerLight: {
+        backgroundColor: '#EBECF0',
+      },
+      button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      logOutButton: {
+        marginTop: 30,
+      },
+      buttons: {
+        height: '45%',
+      }
+})
