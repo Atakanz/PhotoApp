f9fce6d790dd90e98e01da46cac708ebe21e4e3d
Atakanz <atakanzer@gmail.com>
2022-09-24 17:48:13 +0300

    Theme page added.

--- PhotoApp/src/Pages/ThemeSettings/ThemeSettings.js
+++ PhotoApp/src/Pages/ThemeSettings/ThemeSettings.js
@@ -1,13 +1,34 @@
-import React from "react";
-import {SafeAreaView, Text} from "react-native";
+import React from 'react';
+import {SafeAreaView, TouchableOpacity, Text, View} from 'react-native';
 import styles from './ThemeSettings.style';
+import themeSlice from '../../Management/Features/Theme/themeSlice';
+import {useDispatch, useSelector} from 'react-redux';
+import {setTheme} from '../../Management/Features/Theme/themeSlice';
 
-const ThemeSettings = () => {
-    return (
-     <SafeAreaView>
-        <Text>Hello ThemeSettings!</Text>
-     </SafeAreaView>
-    )
-}
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+const ThemeSettings = ({navigation}) => {
+  const dispatch = useDispatch();
+  const theme = useSelector(state => state.theme.theme);
+  const themeToggle = () => {
+    if (theme === 'Light') {
+      dispatch(setTheme('Dark'));
+    } else {
+      dispatch(setTheme('Light'));
+    }
+    // if theme is light, toggle to dark
+  };
+
+  return (
+    <SafeAreaView style={[styles.container, styles[`container${theme}`]]}>
+      <View style={styles.touchableView}>
+        <TouchableOpacity
+          style={[styles.button, styles[`button${theme}`]]}
+          onPress={themeToggle}>
+          <Text style={[styles.text, styles[`text${theme}`]]}>{theme}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default ThemeSettings;
--- PhotoApp/src/Pages/ThemeSettings/ThemeSettings.style.js
+++ PhotoApp/src/Pages/ThemeSettings/ThemeSettings.style.js
@@ -1,3 +1,62 @@
 import {StyleSheet} from "react-native";
 
-export default StyleSheet.create({})
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        height: '100%',
+      },
+      touchableView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 210,
+      },
+      containerLight: {
+        backgroundColor: '#fff',
+      },
+      containerDark: {
+        backgroundColor: '#212121',
+      },
+      button: {
+        borderRadius: 70,
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 140,
+        height: 140,
+      },
+      buttonDark: {
+        borderColor: '#fff',
+      },
+      buttonLight: {
+        borderColor: '#212121',
+      },
+      text: {
+        fontSize: 25,
+        fontWeight: 'bold',
+      },
+      textLight: {
+        color: '#212121',
+      },
+      textDark: {
+        color: '#fff',
+      },
+      ıconButton: {
+        flex: 0,
+        width: 30,
+        height: 30,
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        margin: 0,
+        padding: 0,
+        paddingTop: 0,
+        color: 'white',
+      },
+      backButtonDark: {
+        backgroundColor: '#212121',
+      },
+      backButtonLight: {
+        backgroundColor: '#71a6e2',
+      },
+})
